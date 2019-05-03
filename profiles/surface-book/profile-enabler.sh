1b598f7f46e7c9b2a59134da7ecf8277f0e25659
RononDex <tinoheuberger@hotmail.com>
2019-05-03 19:43:25 +0200

    Added css for lightdm style

--- profiles/surface-book/profile-enabler.sh
+++ profiles/surface-book/profile-enabler.sh
@@ -2,3 +2,4 @@
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 sudo cp $scriptDir/overrides/lightdm-bg.jpg /usr/share/lightdm-webkit/themes/litarvan/images/background.jpg
+sudo cp $scriptDir/overrides/litarvan/styles.css /usr/share/lightdm-webkit/themes/litarvan/styles.css
