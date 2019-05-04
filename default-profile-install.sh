fc2d1062f552d4f915afd27d73b6fe702ff0c8df
RononDex <tinoheuberger@hotmail.com>
2019-05-04 21:55:14 +0200

    Make .xinitrc executable

--- default-profile-install.sh
+++ default-profile-install.sh
@@ -4,3 +4,7 @@ then
     echo "Cloning powerlevel10k"
     git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10k
 fi
+
+
+echo "Updating file permissions ..."
+chmod +x ~/.xinitrc
