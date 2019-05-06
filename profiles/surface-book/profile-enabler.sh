1b4602960722b3d29281219bbbd7df204dda6f95
RononDex <tinoheuberger@hotmail.com>
2019-05-06 21:39:30 +0200

    Add permissions and more constants

--- profiles/surface-book/profile-enabler.sh
+++ profiles/surface-book/profile-enabler.sh
@@ -4,3 +4,6 @@ scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 sudo cp $scriptDir/overrides/lightdm-bg.jpg /usr/share/lightdm-webkit/themes/litarvan/images/background.jpg
 sudo cp $scriptDir/overrides/litarvan/styles.css /usr/share/lightdm-webkit/themes/litarvan/styles.css
 sudo cp $scriptDir/overrides/polybar/constants ~/.config/polybar/constants
+
+currentUser=$(whoami)
+sudo usermod -G lp ${currentUser}
