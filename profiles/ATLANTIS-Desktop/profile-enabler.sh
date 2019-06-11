64d0bdb9f10f9c65e43d992368d71307214f1709
RononDex <tinoheuberger@hotmail.com>
2019-06-11 20:38:00 +0200

    Update profile for desktop

--- profiles/ATLANTIS-Desktop/profile-enabler.sh
+++ profiles/ATLANTIS-Desktop/profile-enabler.sh
@@ -3,7 +3,7 @@ scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 sudo cp $scriptDir/overrides/lightdm-bg.jpg /usr/share/lightdm-webkit/themes/litarvan/images/background.jpg
 sudo cp $scriptDir/overrides/litarvan/styles.css /usr/share/lightdm-webkit/themes/litarvan/styles.css
-sudo cp $scriptDir/overrides/polybar/constants ~/.config/polybar/constants
+cp $scriptDir/overrides/polybar/constants ~/.config/polybar/constants
 sudo cp $scriptDir/overrides/xorg/20-keybord.conf /etc/X11/xorg.conf.d/20-keyboard.conf
 sudo cp $scriptDir/overrides/xorg/21-touchpad.conf /etc/X11/xorg.conf.d/21-touchpad.conf
 
