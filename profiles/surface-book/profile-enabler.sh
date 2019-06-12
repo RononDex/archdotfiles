7c77e2fd541452ad4a99516f77e361bfa47bb0fd
RononDex <tinoheuberger@hotmail.com>
2019-06-12 22:55:24 +0200

    Added more AUR packages, config file for litarvan

--- profiles/surface-book/profile-enabler.sh
+++ profiles/surface-book/profile-enabler.sh
@@ -3,6 +3,7 @@
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 sudo cp $scriptDir/overrides/lightdm.conf /etc/lightdm/lightdm.conf
+sudo cp $scriptDir/overrides/lightdm-webkit2-greeter.conf /etc/lightdm/lightdm-webkit2-greeter.conf
 sudo cp $scriptDir/overrides/lightdm-bg.jpg /usr/share/lightdm-webkit/themes/litarvan/images/background.jpg
 sudo cp $scriptDir/overrides/litarvan/styles.css /usr/share/lightdm-webkit/themes/litarvan/styles.css
 cp $scriptDir/overrides/polybar/constants ~/.config/polybar/constants
@@ -24,6 +25,9 @@ sudo pacman -Sy xf86-input-wacom --noconfirm
 echo "Installing AUR packages..."
 echo "polybar..."
 InstallAurPackage "polybar" "https://aur.archlinux.org/polybar.git"
+InstallAurPackage "msbuild-stable" "https://aur.archlinux.org/msbuild-stable.git"
+InstallAurPackage "signal" "https://aur.archlinux.org/signal.git"
+InstallAurPackage "visual-studio-code-bin.git" "https://aur.archlinux.org/visual-studio-code-bin.git"
 
 currentUser=$(whoami)
 sudo usermod -G lp ${currentUser}
