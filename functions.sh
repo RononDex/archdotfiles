0ef177fe5f55b379e1fa1dd307dc96a9ea175b73
RononDex <RononDex@users.noreply.github.com>
2019-10-28 15:32:42 +0100

    Add new function for Vundle install in vim

--- functions.sh
+++ functions.sh
@@ -60,3 +60,10 @@ MakePackage() {
     cmake ..
     make
 }
+
+BasicVimInstall() {
+    if [ ! -d ~/.vim/bundle/Vundle.vim ]
+    then
+        git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    fi
+}
