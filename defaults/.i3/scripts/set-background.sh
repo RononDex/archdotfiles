9f9e93cb79ad4d11866e3de9b2980b980a81a321
Tino Heuberger <tinoheuberger@protonmail.com>
2019-11-26 17:52:27 +0100

    Add a bit more saturation to generated colors

--- defaults/.i3/scripts/set-background.sh
+++ defaults/.i3/scripts/set-background.sh
@@ -3,4 +3,4 @@
 wallpapersDir="${HOME}/wallpapers/"
 backgroundImage=$(find ${wallpapersDir} | shuf -n 1) # ten random files
 echo $backgroundImage
-wal -i $backgroundImage
+wal -i $backgroundImage --saturate 0.6
