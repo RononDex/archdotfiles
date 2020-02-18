40983d8dc45b2c7cc79bc50e9395affcee660399
Tino Heuberger <tino.heuberger@digitecgalaxus.ch>
2020-02-18 14:54:43 +0100

    Add legacy support for azure devops repositories

--- profiles/DigitecWork/profile-enabler.sh
+++ profiles/DigitecWork/profile-enabler.sh
@@ -6,6 +6,7 @@ sudo cp $scriptDir/overrides/lightdm-webkit2-greeter.conf /etc/lightdm/lightdm-w
 sudo cp $scriptDir/overrides/lightdm-bg.jpg /usr/share/lightdm-webkit/themes/litarvan/images/background.jpg
 sudo cp $scriptDir/overrides/litarvan/styles.css /usr/share/lightdm-webkit/themes/litarvan/styles.css
 cp $scriptDir/overrides/polybar/constants ~/.config/polybar/constants
+cp $scriptDir/overrides/.ssh/config ~/.ssh/config
 sudo cp $scriptDir/overrides/xorg/20-keybord.conf /etc/X11/xorg.conf.d/20-keyboard.conf
 sudo cp $scriptDir/overrides/xorg/20-intel.conf /etc/X11/xorg.conf.d/20-intel.conf
 cp $scriptDir/overrides/polybar/network-traffic.sh ~/.config/polybar/network-traffic.sh
