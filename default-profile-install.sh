3786974cf84ec9bdcf0dc7114250bcddbad1ab13
Tino Heuberger <tino.heuberger@digitecgalaxus.ch>
2020-02-21 17:49:23 +0100

    Include mutt in the default installation

--- default-profile-install.sh
+++ default-profile-install.sh
@@ -42,7 +42,7 @@ sudo pacman -Sy java-runtime-common jre-openjdk ntfs-3g autofs --noconfirm --nee
 sudo pacman -Sy vim bash-completion libftdi ccfits network-manager-applet xorg xorg-xinit lightdm adobe-source-code-pro-fonts --noconfirm --needed
 sudo pacman -Sy python python-pip samba opencv pkgconfig gtest gmock wxgtk2 libmpdclient bc ranger xorg-server binutils keychain --needed --noconfirm
 sudo pacman -Sy htop gtop unzip shadow perl-anyevent-i3 perl-json-xs git-lfs python-pywal fzf arandr --needed --noconfirm
-sudo pacman -Sy zsh-syntax-highlighting openvpn networkmanager-openvpn zathura zathura-cb zathura-pdf-mupdf zathura-ps --needed --noconfirm
+sudo pacman -Sy zsh-syntax-highlighting openvpn networkmanager-openvpn zathura zathura-cb zathura-pdf-mupdf zathura-ps mutt --needed --noconfirm
 
 git lfs install
 git lfs pull
