86f663162c0cf6551c69f675e339aaace4703344
RononDex <tinoheuberger@protonmail.com>
2020-07-07 11:31:23 +0200

    Increase color saturation

--- defaults/.i3/scripts/set-background.sh
+++ defaults/.i3/scripts/set-background.sh
@@ -3,4 +3,4 @@
 wallpapersDir="${HOME}/wallpapers/"
 backgroundImage=$(find ${wallpapersDir} | shuf -n 1) # select random backgroundImage
 echo $backgroundImage
-wal -i $backgroundImage --saturate 0.6
+wal -i $backgroundImage --saturate 0.75
