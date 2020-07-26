3d2f59217fb375d558d58da20f0ee947c6da441d
RononDex <tinoheuberger@protonmail.com>
2020-07-26 14:27:33 +0200

    Add hotspot setup

--- functions/functions.sh
+++ functions/functions.sh
@@ -53,6 +53,36 @@ SetupAutofsForSmbShare() {
     fi
 }
 
+# $1: interface name
+# $2: hotspot name
+# $3: 5GHz (true) or 2.4GHz (false)
+SetupHotspot() {
+    if [ ! -f  ~/.scripts/networking/startHotspot$2 ]; then
+        BAND='a'
+        if $3 -eq true; then $BAND='a'; else $BAND='bg'; fi
+
+        passwd="test"
+        confirmedPasswd="asdf"
+        while [ $passwd -ne $confirmedPasswd ]
+        do
+            echo "Enter password for hotspot ${2}:"
+            stty_orig=`stty -g` # save original terminal setting.
+            stty -echo          # turn-off echoing.
+            read passwd         # read the password
+            stty $stty_orig     # restore terminal setting.
+            echo "Confirm password:"
+            stty_orig=`stty -g` # save original terminal setting.
+            stty -echo          # turn-off echoing.
+            read confirmedPasswd# read the password
+            stty $stty_orig     # restore terminal setting.
+        done
+
+        sh ~/.scripts/networking/setupHotspot $1 $2 $BAND $passwd
+        echo "#!/bin/sh" > ~/.scripts/networking/startHotspot$2
+        echo "nmcli con up ${2}" >> ~/.scripts/networking/startHotspot$2
+    fi
+}
+
 MakePackage() {
     cd ~/packages/$1
     mkdir build
