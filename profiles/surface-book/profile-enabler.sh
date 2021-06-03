43f5169501b320cbe02f020db5fc5041e218629e
RononDex <tinoheuberger@protonmail.com>
2021-06-04 00:18:15 +0200

    Use patched wacom for stylus

--- profiles/surface-book/profile-enabler.sh
+++ profiles/surface-book/profile-enabler.sh
@@ -48,6 +48,7 @@ echo "Turning on dGPU"
 echo "\_SB.PCI0.RP05.HGON" | sudo tee /proc/acpi/call
 
 echo "Installing AUR packages..."
+gpg --keyserver hkps://keyserver.ubuntu.com --recv-key E23B7E70B467F0BF
 InstallAurPackage "libwacom-surface" "https://aur.archlinux.org/libwacom-surface.git"
 InstallAurPackage "nrclient2-free" "https://aur.archlinux.org/nrclient2-free.git"
 InstallAurPackage "nvm" "https://aur.archlinux.org/nvm.git"
